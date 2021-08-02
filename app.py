3ca6971fc83f86245f8e26735a5c006600000165
CampusX <53361867+campusx-official@users.noreply.github.com>
2021-08-02 19:34:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,request,jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def index():
+    data = request.get_json()
+    source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+    amount = data['queryResult']['parameters']['unit-currency']['amount']
+    target_currency = data['queryResult']['parameters']['currency-name']
+
+
+    cf = fetch_conversion_factor(source_currency,target_currency)
+    final_amount = amount * cf
+    final_amount = round(final_amount,2)
+    response = {
+        'fulfillmentText':"{} {} is {} {}".format(amount,source_currency,final_amount,target_currency)
+    }
+    return jsonify(response)
+
+def fetch_conversion_factor(source,target):
+
+    url = "https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey=9aa0c54f5ad4c460c36d".format(source,target)
+
+    response = requests.get(url)
+    response = response.json()
+
+    return response['{}_{}'.format(source,target)]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
